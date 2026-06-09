950a30bf83e7f3a232193b503cfb96c05845323c
micro1007kounou-sudo <micro1007kounou@gmail.com>
2026-06-09 13:31:54 +0900

    refactor: 不要なバッチファイルを削除

--- orijinal-chat/start_serverANDchat.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-
-:: ブラウザを先に開く
-start "" http://localhost:3000
-
-:: CMD を最小化して server.js を実行
-start /min "" cmd /c "node server.js"
-
--- orijinal-chat/stop_server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 65001 >nul
-taskkill /IM node.exe /F
-echo サーバーを停止しました。
-pause
